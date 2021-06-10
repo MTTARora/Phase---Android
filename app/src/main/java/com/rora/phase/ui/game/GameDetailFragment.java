cf3f87fc9b567c8634847f03cc1b10fd0523e8e6
MTTARora <anhmai.rora@gmail.com>
2021-06-10 17:45:24 +0700

    Fix favorite button

--- app/src/main/java/com/rora/phase/ui/game/GameDetailFragment.java
+++ app/src/main/java/com/rora/phase/ui/game/GameDetailFragment.java
@@ -217,11 +217,13 @@ public class GameDetailFragment extends BaseFragment {
         btnFavorite.setOnClickListener(v -> {
             showLoadingScreen();
             userViewModel.updateFavorite(game, (err, data) -> {
-                if (err != null && !err.isEmpty())
+                if (err != null && !err.isEmpty()) {
                     if (!err.equals("401"))
                         getActivity().runOnUiThread(() -> Toast.makeText(getContext(), err, Toast.LENGTH_LONG).show());
+                }
                 else
                     btnFavorite.setImageResource(game.getFavorited() ? R.drawable.ic_favorite : R.drawable.ic_unfavorite);
+
                 hideLoadingScreen();
             });
         });
